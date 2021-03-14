a2d087bc228a69363a865dea888af90bb8b497bb
Ali Nategh <a.nategh76@gmail.com>
2021-03-14 18:44:48 +0330

    Added Singleton Pattern example

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = Singleton.GetInstance;
+            var s2 = Singleton.GetInstance;
+            if (s1 == s2) Console.WriteLine("s1 and s2 are both the same instance of the Singleton class.");
+
+            var x = s1.Add(6, 2);
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ SingletonDemo/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDemo
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance = null;
+        private static int counter = 0;
+        private static readonly object padlock = new object();
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine($"Singleton has been instantiated {counter} times.");
+            // this is to check if we are violating Singleton pattern.
+        }
+        public static Singleton GetInstance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public decimal Add(decimal a, decimal b) => a + b;
+    }
+}
